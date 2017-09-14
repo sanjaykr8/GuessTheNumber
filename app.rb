9e1b128772d4420bc5e8069315da2acb0e0a0f63
Sanjaykumar Ranganayakulu <sanjaykr@andrew.cmu.edu>
2017-09-13 22:37:40 -0400

    folderChange

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+
+enable :sessions
+#session["user_guess"] = 0
+# total_users = 0
+n = rand 1..10
+
+get '/' do
+  # if session[“user_guess”].nil?
+    #  session[“user_guess”] = 0
+  #  end
+  #total_users = 0
+
+  "Guess a number between 1 and 10......Ready......Set......GO"
+end
+
+get '/:guess' do
+  #total_users += 1
+
+  if session["user_guess"].nil?
+     session["user_guess"] = 0
+  end
+
+  if session["user_guess"] < 2
+    if params[:guess].to_i == n
+      return "You Won!!"
+    else
+      session["user_guess"] += 1
+      return "Try again, you have #{3-session["user_guess"]} chances"
+    end
+  else
+    return "Game Over!!!, the correct guess is #{n}"
+  end
+end
