9fac6f08b5e939b741bc80a49ba9ba47af7dca29
sanjaykr8 <sanjaykr@andrew.cmu.edu>
2017-09-17 13:25:38 -0400

    update

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "sinatra/reloader" if development?
 
 
 enable :sessions
+n = rand 1..10
 
 get '/' do
   "Guess a number between 1 and 10......Ready......Set......GO"
@@ -10,20 +11,23 @@ end
 
 get '/:guess' do
 
-  if session["user_guess"].nil?
-     n = rand 1..10
-     session["user_guess"] = 0
+  if session[:attempt].nil?
+     session[:attempt] = 0
   end
 
-  if session["user_guess"] < 2
-    if params[:guess].to_i == n
-      return "You Won!!"
+    if session[:attempt] < 3
+
+            session[:attempt] += 1
+            if params[:guess].to_i == n
+              return "You Won!!"
+
+            else
+              remainingAttempts = 3 - session[:attempt].to_i
+              return "Try again, you have #{remainingAttempts} more attempts"
+            end
+
     else
-      session["user_guess"] += 1
-      return "Try again, you have #{3-session["user_guess"]} chances"
-    end
-  else
-    session[:failedCounter] += 1
-    return "Game Over!!!, the correct guess is #{n}. Total number of failed attempts are #{session[:failedCounter].to_s} "
+      return "Game Over!!!, the correct guess is #{n}."
   end
+
 end
